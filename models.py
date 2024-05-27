43487c7aab2c5822dc7935509e1407950fb2b743
arthurlorenzon <arthurgabriellp@gmail.com>
2024-05-26 22:45:45 -0300

    Criado arquivo models e adicionado funções para iniciar o objeto, formatar cpf e validar cpf, email e data de nascimento

---
+++ models.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import re
+from datetime import datetime
+
+
+class Salesperson:
+    def __init__(self, name, cpf, birth_date, email, state):
+        self.name = name
+
+        if self.validade_cpf(cpf):
+            self.cpf = cpf
+        else:
+            raise ValueError("CPF inválido")
+
+        if self.validate_birth_date(birth_date):
+            self.birth_date = birth_date
+        else:
+            raise ValueError("Data de nascimento inválida")
+
+        if self.validate_email(email):
+            self.email = email
+        else:
+            raise ValueError("E-mail inválido")
+
+        self.state = state
+        self.sales = []
+
+    def format_cpf(self, cpf):
+        cpf_digits = ''.join(filter(str.isdigit, cpf))
+        formatted_cpf = '{}.{}.{}-{}'. format(
+            cpf_digits[:3], cpf_digits[3:6], cpf_digits[6:9], cpf_digits[9:])
+        return formatted_cpf
+
+    def validade_cpf(self, cpf):
+        cpf_digits = ''.join(filter(str.isdigit, cpf))
+        return len(cpf_digits) == 11
+
+    def validate_email(self, email):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        return re.match(pattern, email) is not None
+
+    def validate_birth_date(self, birth_date):
+        try:
+            datetime.strptime(birth_date, "%d/%m/%Y")
+            return True
+        except ValueError:
+            return False
