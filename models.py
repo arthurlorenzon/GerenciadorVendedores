845bcb5a369fd35c67bf35e2754bc58c9c13b914
arthurlorenzon <arthurgabriellp@gmail.com>
2024-05-26 22:53:11 -0300

    Adicionado funções para exportar os dados para um arquivo excel a classe SalespersonManagement

--- models.py
+++ models.py
@@ -82,3 +82,67 @@ class SalespersonManagement:
     def delete_salesperson(self, cpf):
         if cpf in self.salespersons:
             del self.salespersons[cpf]
+
+    def add_sale(self, cpf, valor, channel):
+        if cpf in self.salespersons:
+            salesperson = self.salespersons[cpf]
+            salesperson.add_sale(valor, channel)
+
+    def export_excel(self, file_name):
+        salesperson_data = {
+            'Nome do Vendedor': [],
+            'CPF': [],
+            'Data de Nascimento': [],
+            'E-mail': [],
+            'Estado (UF)': []
+        }
+
+        sales_data = {
+            'Nome do Vendedor': [],
+            'CPF': [],
+            'Estado (UF)': [],
+            'Valor da Venda': [],
+            'Canal da Venda': [],
+            'Comissão': []
+        }
+
+        payments_data = {
+            'Nome do Vendedor': [],
+            'Comissão': []
+        }
+
+        for salesperson in self.salespersons.values():
+            salesperson_data['Nome do Vendedor'].append(salesperson.name)
+            salesperson_data['CPF'].append(salesperson.cpf)
+            salesperson_data['Data de Nascimento'].append(
+                salesperson.birth_date)
+            salesperson_data['E-mail'].append(salesperson.email)
+            salesperson_data['Estado (UF)'].append(salesperson.state)
+
+            total_comission = 0
+
+            for sale in salesperson.sales:
+                sales_data['Nome do Vendedor'].append(salesperson.name)
+                sales_data['CPF'].append(salesperson.cpf)
+                sales_data['Estado (UF)'].append(salesperson.state)
+                sales_data['Valor da Venda'].append(sale['valor'])
+                sales_data['Canal da Venda'].append(sale['canal'])
+                sales_data['Comissão'].append(sale['comissao'])
+
+                total_comission += sale['comissao']
+
+            if total_comission >= 1000:
+                total_comission *= 0.9
+
+            payments_data['Nome do Vendedor'].append(salesperson.name)
+            payments_data['Comissão'].append(total_comission)
+
+        with pd.ExcelWriter(file_name) as writer:
+            df_salespersons = pd.DataFrame(salesperson_data)
+            df_sales = pd.DataFrame(sales_data)
+            df_payments = pd.DataFrame(payments_data)
+            df_salespersons.to_excel(writer, index=False,
+                                     sheet_name='Vendedores')
+            df_sales.to_excel(writer, index=False, sheet_name='Vendas')
+            df_payments.to_excel(writer, index=False,
+                                 sheet_name='Pagamentos')
